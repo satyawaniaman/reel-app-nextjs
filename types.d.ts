dfb779524f0c575b4d206c6ebb80818aaab0e5ae
aman satyawani <amansatyawani10@gmail.com>
2025-02-01 03:11:13 +0530

    database connection initialized.

---
+++ types.d.ts
@@ -0,0 +1,9 @@
+import { Connection } from "mongoose";
+declare global {
+  // eslint-disable-next-line no-var
+  var mongoose: {
+    conn: Connection | null;
+    promise: Promise<Connection> | null;
+  };
+}
+export {};
